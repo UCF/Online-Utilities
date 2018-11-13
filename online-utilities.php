4cd5a58e1b88b32cbc3b0fedb069806a307831bb
Jo Dickson <jogreybill@gmail.com>
2018-11-13 16:10:11 -0500

    Initial plugin template file setup

--- online-utilities.php
+++ online-utilities.php
@@ -1,10 +1,13 @@
 <?php
 /*
 Plugin Name: Online Utilities
+Description: Functionality and utilities for the UCF Online website
 Version: 2.0.0
 Author: UCF Web Communications
-Description: Functionality and utilities for the UCF Online website
+License: GPL3
+GitHub Plugin URI: UCF/Online-Utilities
 */
+
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
